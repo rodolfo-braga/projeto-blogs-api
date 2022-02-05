77a6dc1a7fb531919078848961f607c393c76e40
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-05 19:37:30 -0300

    feat: Cria middleware de erro

---
+++ middlewares/error.js
@@ -0,0 +1,15 @@
+module.exports = (err, req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(400)
+      .json({ message: err.details[0].message });
+  }
+
+  const statusByErrorCode = {
+    notFound: 404,
+    conflict: 409,
+  };
+
+  const status = statusByErrorCode[err.code] || 500;
+
+  res.status(status).json({ message: err.message });
+};
