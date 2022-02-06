27584715000e3c9b6a07b583ae775da3571771dd
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-06 15:13:41 -0300

    feat(services): Cria função getCategories

--- services/categoriesService.js
+++ services/categoriesService.js
@@ -6,12 +6,19 @@ const create = async (category) => {
   if (existingCategory) {
     return { error: { code: 'conflict', message: 'Category already registered' } };
   }
-  
+
   const newCategory = await Category.create(category);
 
   return newCategory;
 };
 
+const getCategories = async () => {
+  const categories = await Category.findAll();
+
+  return categories;
+};
+
 module.exports = {
   create,
+  getCategories,
 };
