6c118ff2de1a19d7db53b20339d43e6375642898
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-06 13:28:06 -0300

    feat(controllers): Cria rota GET /user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const users = require('express').Router();
 const rescue = require('express-rescue');
 const jwt = require('jsonwebtoken');
+const validateJWT = require('../auth/validateJWT');
 
 const usersService = require('../services/usersService');
 const { userSchema } = require('../validation/schemas');
@@ -20,6 +21,12 @@ users.post('/', rescue(async (req, res, next) => {
   const token = jwt.sign({ data: newUser }, process.env.JWT_SECRET, jwtConfig);
 
   return res.status(201).json({ token });
+}));
+
+users.get('/', validateJWT, rescue(async (req, res) => {
+  const allUsers = await usersService.getUsers();
+
+  return res.status(200).json(allUsers);
 }));
 
 module.exports = users;
